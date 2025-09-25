eefb4c57b057c52fce5cf20679288c3adca80f27
dajun2030 <you@example.comxingjun2030@gmail.com>
2025-09-25 20:11:01 +0800

    Add: 26.Remove Duplicates - Counting Cover Method

--- easy/leetcode16removesdupilicatesfromsortedarray.py
+++ easy/leetcode16removesdupilicatesfromsortedarray.py
@@ -1,9 +1,27 @@
+#双指针法
+# def removeDuplicates(nums) :
+#     slow=0
+#     for fast in range(1, len(nums)):
+#         if nums[slow]!=nums[fast]:
+#             slow+=1
+#             nums[slow]=nums[fast]
+#     return slow+1
+
+#计数覆盖法
+# 核心思路
+# 与双指针法类似，但更直观：显式记录当前唯一元素的值，当遇到不同值时才进行覆盖。
+
 def removeDuplicates(nums) :
-    slow=0
-    for fast in range(1, len(nums)):
-        if nums[slow]!=nums[fast]:
-            slow+=1
-            nums[slow]=nums[fast]
-    return slow+1
+    if not nums:
+        return 0
+    current=nums[0]
+    count=1
+    for i in range(1,len(nums)):
+        if nums[i]!=current:
+            nums[count]=nums[i]
+            count+=1
+            current=nums[i]
+    return count
+
 
 print(removeDuplicates([0,0,1,1,1,2,2,3,3,4,4,55,6,7,8,8,8,8,8,9]))
